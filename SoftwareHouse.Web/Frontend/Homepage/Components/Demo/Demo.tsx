219bc6daf8fbda52baccd879ead27e82bd0fc065
darekmaciag <darekmaciag@gmail.com>
2018-03-29 00:23:21 +0200

    Add project files.

---
+++ SoftwareHouse.Web/Frontend/Homepage/Components/Demo/Demo.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from 'react';
+import './Demo.scss';
+
+
+class Demo extends React.Component<any, any> {
+
+    public render() {
+        return (
+            <div className="Demo">
+                <p>ASP.NET + React, from PoznajProgramowanie.pl</p>
+            </div>
+        )
+    }
+}
+
+export default Demo;
---
+++ SoftwareHouse.Web/Frontend/Homepage/index.tsx
@@ -0,0 +1,10 @@
+﻿import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import './Demo.scss';
+
+import Demo from './Components/Demo/Demo';
+
+ReactDOM.render(
+    <Demo />,
+    document.getElementById('react-homepage-root')
+);
