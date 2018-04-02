3b6205c8ac0423d0074a5fbad77de74e1e448851
darekmaciag@wp.pl <darekmaciag@wp.pl>
2018-04-03 01:53:19 +0200

    a

---
+++ SoftwareHouse.Web/Frontend/Homepage/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ReactDOM = require("react-dom");
+var React = require("react");
+require("./Demo.scss");
+var Demo_1 = require("./Components/Demo/Demo");
+ReactDOM.render(React.createElement(Demo_1.default, null), document.getElementById('react-homepage-root'));
+if (module.hot) {
+    module.hot.accept();
+}
+//# sourceMappingURL=index.js.map
